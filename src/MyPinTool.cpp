d88f43e2b2217844de9b42cdea9f7d958ca2193c
Elias Boutaleb <e.boutaleb@hotmail.fr>
2016-04-02 00:46:42 +0200

    Add filter

--- src/MyPinTool.cpp
+++ src/MyPinTool.cpp
@@ -4,12 +4,35 @@
 #include <iomanip>
 #include <fstream>
 #include <algorithm>
+#include <map>
 
 std::ostream *out = &cerr;
 
+ADDRINT main_begin;
+ADDRINT main_end;
+bool main_reached=false;
+INT64 logfilter=1;
+ADDRINT filter_begin=0;
+ADDRINT filter_end=0;
+
+struct moduledata_t
+{
+    BOOL excluded;
+    ADDRINT begin;
+    ADDRINT end;
+};
+
+typedef std::map<string, moduledata_t> modmap_t;
+modmap_t mod_data;
+
 // Command line switches
 KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE,  "pintool",
-    "o", "res.out", "specify file name for MyPinTool output");
+    "o", "res.out", "specify file name for UBW output");
+
+KNOB<string> KnobLogFilter(KNOB_MODE_WRITEONCE, "pintool",
+    "f", "1", "(0) no filter\
+                (1) filter system libraries\
+                (2) filter all but main exec (0x400000-0x410000) trace only specified address range");
 
 // Utilities
 /*!
@@ -22,6 +45,46 @@ INT32 Usage()
     cerr << KNOB_BASE::StringKnobSummary() << endl;
 
     return -1;
+}
+
+BOOL ExcludedAddress(ADDRINT ip)
+{
+    switch (logfilter)
+    {
+    case 1:
+        if (! main_reached)
+        {
+	        // Filter loader before main
+	        if ((ip < main_begin) || (ip > main_end))
+	            return TRUE;
+            else
+                main_reached=true;
+		}
+        if ((ip >= main_begin) && (ip <= main_end))
+            return FALSE;
+        for(modmap_t::iterator it = mod_data.begin(); it != mod_data.end(); ++it)
+        {
+            if(it->second.excluded == FALSE) continue;
+            /* Is the EIP value within the range of any excluded module? */
+            if(ip >= it->second.begin && ip <= it->second.end) return TRUE;
+        }
+        break;
+    case 2:
+    {
+        PIN_LockClient();
+        IMG im = IMG_FindByAddress(ip);
+        PIN_UnlockClient();
+        if (! IMG_Valid(im) || ! IMG_IsMainExecutable(im))
+            return TRUE;
+        break;
+    }
+    case 3:
+        return ((ip < filter_begin) || (ip > filter_end));
+        break;
+    default:
+        break;
+    }
+    return FALSE;
 }
 
 /**
@@ -71,10 +134,10 @@ std::string dumpInstruction(INS ins)
 {
     std::stringstream ss;
 
-    ADDRINT address = INS_Address(ins);
+    ADDRINT ceip = INS_Address(ins);
 
     // Generate address and module information
-    ss << getModule(address) << "  " << setfill('0') << setw(8) << uppercase << hex << address;
+    ss << getModule(ceip) << "  " << setfill('0') << setw(8) << uppercase << hex << ceip;
     ss << "   ";
 
     // Generate diassembled string
@@ -117,9 +180,12 @@ void dump_shellcode(std::string* instructionString, CONTEXT* ctxt)
 
 void traceInst(INS ins, VOID*)
 {
-    ADDRINT address = INS_Address(ins);
+    ADDRINT ceip = INS_Address(ins);
 
-    std::string mod_name = getModule( address );
+    if(ExcludedAddress(ceip))
+        return;
+
+    std::string mod_name = getModule(ceip);
 
     INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(dump_shellcode),
             IARG_PTR, new std::string(dumpInstruction(ins)),
@@ -138,6 +204,9 @@ VOID Trace(TRACE trace, VOID *v)
 {
     // Visit every basic block in the trace
     for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl)) {
+        INS head = BBL_InsHead(bbl);
+        if(ExcludedAddress(INS_Address(head)))
+            return;
 
         for (INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins)) {
             traceInst(ins, trace);
@@ -158,7 +227,41 @@ int main(int argc, char *argv[])
     // in the command line or the command line is invalid
     if( PIN_Init(argc,argv) )
     {
-        return Usage();
+       return Usage();
+    }
+
+    char *endptr;
+    const char *tmpfilter = KnobLogFilter.Value().c_str();
+    logfilter=strtoull(tmpfilter, &endptr, 16);
+    if (endptr == tmpfilter) {
+        cerr << "ERR: Failed parsing option -f" <<endl;
+        return 1;
+    }
+    if ((endptr[0] == '\0') && (logfilter > 2)) {
+        cerr << "ERR: Failed parsing option -f" <<endl;
+        return 1;
+    }
+    if (logfilter > 2) {
+        filter_begin=logfilter;
+        logfilter = 3;
+        char *endptr2;
+        if (endptr[0] != '-') {
+            cerr << "ERR: Failed parsing option -f" <<endl;
+            return 1;
+        }
+        filter_end=strtoull(endptr+1, &endptr2, 16);
+        if (endptr2 == endptr+1) {
+            cerr << "ERR: Failed parsing option -f" <<endl;
+            return 1;
+        }
+        if (endptr2[0] != '\0') {
+            cerr << "ERR: Failed parsing option -f" <<endl;
+            return 1;
+        }
+        if (filter_end <= filter_begin) {
+            cerr << "ERR: Failed parsing option -f" <<endl;
+            return 1;
+        }
     }
 
     string fileName = KnobOutputFile.Value();
